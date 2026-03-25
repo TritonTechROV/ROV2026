b3ee9a245bea7bd20f8ae90923c873d8e9709316
Chucklee1 <kermitthefrog@kakao.com>
2026-03-25 11:19:09 -0700

    added:   flake for development environment new file:   flake.lock new file:   flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Development shell for declarative nix environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs = {self, nixpkgs}: let
+    genSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+     setPkgs = system: import nixpkgs {inherit system;};
+
+    allSystems = f: genSystems (system:
+    let
+      pkgs = setPkgs system;
+    in f system pkgs);
+  in {
+    devShells = allSystems (system: pkgs: {
+      default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          opencv
+          platformio
+        ];
+      };
+    });
+  };
+}
